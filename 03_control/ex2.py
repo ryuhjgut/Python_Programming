b779412141cc68aea2dbe60c01e0f79d6f8afc1d
Arcana <ryuhjgut1017@gmail.com>
2026-09-03 15:31:11 +0900

    new

---
+++ 03_control/ex2.py
@@ -0,0 +1,40 @@
+# 반복문 : while, for
+
+# while문
+# 1~10까지 반복 출력
+i = 1
+while i <= 10:
+    print(i)
+    i += 1
+    if i == 5:
+        break
+else:
+    print("END")
+
+nums = [1, 3, 5, 7, 9]
+target = 2
+
+while i < len(nums):
+    if nums[9] == target:
+        print(f"{target} found.")
+        found = True
+        break
+    i += 1
+else:
+    print(f"{target} not found.")
+
+# if not found:
+# print(f"{target} not found.)
+
+# 1 ~ 10 까지의 합
+i = 1
+tot = 0
+
+while i <= 10:
+    i += 1
+    if i % 2 == 1:
+        continue
+    tot += 1
+
+else:
+    print(f"합: {tot}")
